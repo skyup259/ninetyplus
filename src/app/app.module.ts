39c426a1acdda530ada7a0f26b61693f68d3e496
Saurabh Sunny <skumar@aperturecvo.com>
2020-09-04 11:21:41 +0530

    added section: teachers, students, alumni and notice board

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppComponent } from './app.component';
+import { TeacherComponent } from './component/teacher/teacher.component';
+import { StudentComponent } from './component/student/student.component';
+import { AlumniComponent } from './component/alumni/alumni.component';
+import { NoticeComponent } from './component/notice/notice.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { SlidecardComponent } from './component/slidecard/slidecard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeacherComponent,
+    StudentComponent,
+    AlumniComponent,
+    NoticeComponent,
+    NavbarComponent,
+    SlidecardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
